f0efc2fa70ab9bd51b5d0b8df6ea64c108e072fa
Dan <danalk61@gmail.com>
2021-02-07 23:51:04 +0000

    Merge branch 'dev' of into dev

---
+++ server/pages/api/otc/askbob/[otc].js
@@ -0,0 +1,126 @@
+// Watson Routes
+
+import Cors from "cors";
+import SpeechToTextV1 from "ibm-watson/speech-to-text/v1";
+import TextToSpeechV1 from "ibm-watson/text-to-speech/v1";
+import { IamAuthenticator } from "ibm-watson/auth";
+import { connectToDatabase } from "../../../../utils/mongodb";
+
+const cors = Cors({
+  methods: ["GET", "HEAD", "POST"],
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+const tts = new TextToSpeechV1({
+  authenticator: new IamAuthenticator({
+    apikey: process.env.TTS_API_KEY,
+  }),
+  url: process.env.TTS_ENDPOINT,
+});
+
+const tts_options = {
+  text: "",
+  accept: "audio/wav",
+  voice: "en-US_MichaelVoice",
+};
+
+const handler = async (req, res) => {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  const { body, method, query } = req;
+  const { otc: rawOtc } = query;
+
+  const otc = rawOtc
+    .trim()
+    .split(/[\s-]+/)
+    .join("")
+    .toLowerCase();
+
+  const { client } = await connectToDatabase();
+  const db = await client.db(process.env.MONGODB_DB);
+  const usersDB = await db.collection("users");
+
+  const users = await usersDB.find({}).toArray();
+  const user = users.find(
+    (u) =>
+      u.consumers.findIndex((c) => {
+        return c.otc.split("-").join("") === otc;
+      }) != -1
+  );
+
+  if (!user)
+    return res
+      .status(403)
+      .json({ message: "You don't have access to this page" });
+
+  const consumer = user.consumers.find(
+    (c) => c.otc.split("-").join("") === otc
+  );
+
+  if (consumer) {
+    switch (method) {
+      case "POST":
+        // ---------------- POST
+        try {
+          const { text } = body;
+          const response = await fetch(`${process.env.ASKBOB_ENDPOINT}/query`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: `message=${text}&sender=${user.name}`,
+          }).then((r) => {
+            if (r.ok) {
+              return r.json();
+            }
+            throw r;
+          });
+
+          const messages = response ? response.messages : [];
+
+          tts_options.text = messages.length
+            ? messages[0]
+            : "Bob could not understand";
+
+          const speech_res = await tts.synthesize(tts_options);
+          const buffer = await tts.repairWavHeaderStream(speech_res.result);
+
+          res.setHeader("Content-Type", "audio/wav");
+          res.status(200).send(buffer);
+        } catch (err) {
+          console.error(`api.otc.askbob.POST: ${err}`);
+          return res
+            .status(500)
+            .json({ message: "Uncaught Server Error", data: err });
+        }
+        break;
+      case "GET":
+      // ---------------- GET
+      case "PUT":
+      // ---------------- PUT
+      case "DELETE":
+      // ---------------- DELETE
+      default:
+        return res.status(405).json({ message: "This route does not exist" });
+        break;
+    }
+  } else {
+    return res
+      .status(403)
+      .json({ message: "You don't have access to this page" });
+  }
+};
+
+export default handler;
