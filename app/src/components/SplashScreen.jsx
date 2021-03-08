34a6701743c00866b571df7c563b998896830a48
Radu <radu.echim.19@ucl.ac.uk>
2021-03-08 15:53:55 +0200

    feat #70: Added spinner to splash screen

--- app/src/components/SplashScreen.jsx
+++ app/src/components/SplashScreen.jsx
@@ -1,8 +1,8 @@
-import React from "react"
-import { Flex, Stack, Icon, Text } from "@chakra-ui/core";
+import React from "react";
+import { Flex, Stack, Icon, Text, Spinner } from "@chakra-ui/core";
 
 const SplashScreen = () => (
-    <Flex
+  <Flex
     direction="column"
     position="fixed"
     justify="center"
@@ -15,9 +15,11 @@ const SplashScreen = () => (
     top="0"
     left="0"
   >
-    <Text textAlign="center" color="white" >by UCL FISE Groups</Text>
+    <Text textAlign="center" color="white">
+      by UCL FISE Groups
+    </Text>
     <Stack>
-      <a >
+      <a>
         <Icon name="ibm" color="white" w="200px" h="50px" m="1rem" />
       </a>
       <a href="https://nhs.uk">
@@ -35,6 +37,13 @@ const SplashScreen = () => (
         Supervised by John MacNamara, Dean Mohammedally
       </Text>
     </Stack>
+    <Spinner
+      size="xl"
+      color="blue.500"
+      mt="2rem"
+      thickness="5px"
+      speed="0.6s"
+    />
   </Flex>
 );
 
