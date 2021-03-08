3c90d56d73e01808cf65f6b374e2f93017af9352
Dan <danalk61@gmail.com>
2021-03-08 00:07:28 +0000

    feat #46 Dynamic (text) profile picture editing

--- server/pages/dashboard/contact/[contact_id].jsx
+++ server/pages/dashboard/contact/[contact_id].jsx
@@ -31,7 +31,7 @@ import { Footer } from "../../../components/Footer";
 import Breadcrumbs from "../../../components/Breadcrumbs";
 import Loading from "../../../components/Loading";
 import Checkbox from "../../../components/Checkbox";
-import * as yup from 'yup';
+import * as yup from "yup";
 
 const MakeChangesForm = ({
   router,
@@ -42,12 +42,17 @@ const MakeChangesForm = ({
   currentPhone,
   currentProfileImage,
 }) => {
-  var hasSelectedDeleteImage = false; // CHANGED NAME FROM deleteImg 
-  const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/gif', 'image/png'];
+  var hasSelectedDeleteImage = false; // CHANGED NAME FROM deleteImg
+  const SUPPORTED_FORMATS = [
+    "image/jpg",
+    "image/jpeg",
+    "image/gif",
+    "image/png",
+  ];
   const fileToBase64 = (inputFile) => {
-    const fileReader = new FileReader() 
+    const fileReader = new FileReader();
 
-    return new Promise((resolve, reject) => {   
+    return new Promise((resolve, reject) => {
       fileReader.onerror = () => {
         fileReader.abort();
         reject(new DOMException("Problem parsing background file."));
@@ -64,8 +69,8 @@ const MakeChangesForm = ({
     if (values.profileImage) {
       values.profileImage = await fileToBase64(values.profileImage);
     } else if (values.hasSelectedDeleteImage) {
-      //delete image if selected 
-      values.profileImage = ""; 
+      //delete image if selected
+      values.profileImage = "";
     } else {
       values.profileImage = currentProfileImage;
     }
@@ -122,10 +127,16 @@ const MakeChangesForm = ({
       }}
       onSubmit={handleFormSubmit}
       validationSchema={yup.object().shape({
-        profileImage: yup.mixed()
+        profileImage: yup
+          .mixed()
           .nullable()
           .notRequired()
-          .test('fileType', "Unsupported File Format", value => !value || (value && SUPPORTED_FORMATS.includes(value.type))),
+          .test(
+            "fileType",
+            "Unsupported File Format",
+            (value) =>
+              !value || (value && SUPPORTED_FORMATS.includes(value.type))
+          ),
       })}
     >
       {({
@@ -177,41 +188,88 @@ const MakeChangesForm = ({
                   What relation is this person to ?
                 </FormLabel>
                 <Select {...field} id="relation">
-                <option default style={{ color: 'black' }}>Select Relation</option>
-                <option value="son" style={{ color: 'black' }} >Their son</option>
-                <option value="daughter" style={{ color: 'black' }} >Their daughter</option>
-                <option value="grandson" style={{ color: 'black' }}>Their grandson</option>
-                <option value="granddaughter" style={{ color: 'black' }}>Their granddaughter</option>
-                <option value="father" style={{ color: 'black' }}>Their father</option>
-                <option value="mother" style={{ color: 'black' }}>Their mother</option>
-                <option value="grandfather" style={{ color: 'black' }}>Their grandfather</option>
-                <option value="grandmother" style={{ color: 'black' }}>Their grandmother</option>
-                <option value="uncle" style={{ color: 'black' }}>Their uncle</option>
-                <option value="aunt" style={{ color: 'black' }}>Their aunt</option>
-                <option value="brother" style={{ color: 'black' }}>Their brother</option>
-                <option value="sister" style={{ color: 'black' }}>Their sister</option>
-                <option value="friend" style={{ color: 'black' }}>Their friend</option>
+                  <option default style={{ color: "black" }}>
+                    Select Relation
+                  </option>
+                  <option value="son" style={{ color: "black" }}>
+                    Their son
+                  </option>
+                  <option value="daughter" style={{ color: "black" }}>
+                    Their daughter
+                  </option>
+                  <option value="grandson" style={{ color: "black" }}>
+                    Their grandson
+                  </option>
+                  <option value="granddaughter" style={{ color: "black" }}>
+                    Their granddaughter
+                  </option>
+                  <option value="father" style={{ color: "black" }}>
+                    Their father
+                  </option>
+                  <option value="mother" style={{ color: "black" }}>
+                    Their mother
+                  </option>
+                  <option value="grandfather" style={{ color: "black" }}>
+                    Their grandfather
+                  </option>
+                  <option value="grandmother" style={{ color: "black" }}>
+                    Their grandmother
+                  </option>
+                  <option value="uncle" style={{ color: "black" }}>
+                    Their uncle
+                  </option>
+                  <option value="aunt" style={{ color: "black" }}>
+                    Their aunt
+                  </option>
+                  <option value="brother" style={{ color: "black" }}>
+                    Their brother
+                  </option>
+                  <option value="sister" style={{ color: "black" }}>
+                    Their sister
+                  </option>
+                  <option value="friend" style={{ color: "black" }}>
+                    Their friend
+                  </option>
                 </Select>
                 <FormErrorMessage>{form.errors.relation}</FormErrorMessage>
               </FormControl>
             )}
           </Field>
-          
+
           <br />
-          <FormLabel>Current Profile Picture</FormLabel>
-          <img src={currentProfileImage} width="100px" height="100x" />
-          <Field
-            name="hasSelectedDeleteImage"
-            type="checkbox"
-            checked={values.hasSelectedDeleteImage === true}
-            label="Remove"
-            component={Checkbox}
+          {currentProfileImage && (
+            <FormLabel>Current Profile Picture</FormLabel>
+          )}
+          {currentProfileImage && (
+            <img src={currentProfileImage} width="100px" height="100x" />
+          )}
+          {currentProfileImage && (
+            <Field
+              name="hasSelectedDeleteImage"
+              type="checkbox"
+              checked={values.hasSelectedDeleteImage === true}
+              label="Remove"
+              component={Checkbox}
+            />
+          )}
+          {currentProfileImage ? (
+            <FormLabel htmlFor="profileImage">Change Profile Picture</FormLabel>
+          ) : (
+            <FormLabel htmlFor="profileImage">
+              Upload a Profile Picture
+            </FormLabel>
+          )}
+          <br />
+          <input
+            id="profileImage"
+            name="profileImage"
+            type="file"
+            accept="image/*"
+            onChange={(event) => {
+              setFieldValue("profileImage", event.currentTarget.files[0]);
+            }}
+            className="form-control"
           />
-          <FormLabel htmlFor="profileImage">Change Profile Picture</FormLabel>
-          <br />
-          <input id="profileImage" name="profileImage" type="file" accept="image/*" onChange={(event) => {
-            setFieldValue("profileImage", event.currentTarget.files[0]);
-          }} className="form-control" />
           <br />
 
           <Button
@@ -301,15 +359,16 @@ const ContactPage = () => {
             variantColor="red"
             onClick={handleDeleteContact}
           >
-            Delete {capitalize(contact.name)} from {capitalize(contact.consumer_name)}'s Profile
+            Delete {capitalize(contact.name)} from{" "}
+            {capitalize(contact.consumer_name)}'s Profile
           </Button>
         </Box>
       </Main>
       <Footer />
     </Container>
   ) : (
-      <Loading />
-    );
+    <Loading />
+  );
 };
 
 export default ContactPage;
