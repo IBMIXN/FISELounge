21fcdaeeb3cd3d15f8c33b029e373e310b8ec49d
Dan <danalk61@gmail.com>
2021-03-19 00:19:24 +0000

    feat #71 footer and splashsceen updates

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -13,18 +13,19 @@ const Footer = (props) => (
   >
     <Text textAlign="center">FISE Lounge was created by students at UCL</Text>
     <Stack>
-      <a href="https://ibm.com">
+      <a href="https://ibm.com" aria-label="ibm">
         <Icon name="ibm" color="grey.900" w="200px" h="50px" m="1rem" />
       </a>
-      <a href="https://nhs.uk">
+      <a href="https://nhs.uk" aria-label="nhs">
         <Image
+          alt="nhs"
           src="/icons/nhs-inx-logo.png"
           w="157px"
           h="81px"
           mx="auto"
         ></Image>
       </a>
-      <a href="https://ucl.ac.uk">
+      <a href="https://ucl.ac.uk" aria-label="ucl">
         <Icon name="ucl" color="gray.700" w="200px" h="50px" m="1rem" />
       </a>
     </Stack>
--- app/src/components/SplashScreen.jsx
+++ app/src/components/SplashScreen.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Stack, Icon, Text, Spinner, Image } from "@chakra-ui/core";
+import { Flex, Stack, Icon, Text, Spinner } from "@chakra-ui/core";
 
 const SplashScreen = () => (
   <Flex
@@ -15,24 +15,6 @@ const SplashScreen = () => (
     top="0"
     left="0"
   >
-    <Text textAlign="center" color="white">
-      by UCL FISE Groups
-    </Text>
-    <Stack>
-      <Icon name="ibm" color="white" w="200px" h="50px" m="1rem" />
-      <a href="https://nhs.uk">
-        <Image
-          src="/icons/nhs-inx-logo.png"
-          w="157px"
-          h="81px"
-          mx="auto"
-          mb="1.2rem"
-          mt="1.2rem"
-        ></Image>
-      </a>
-      <Icon name="ucl" color="gray.400" w="200px" h="50px" m="1rem" />
-    </Stack>
-
     <Spinner
       size="xl"
       color="blue.500"
@@ -41,12 +23,30 @@ const SplashScreen = () => (
       speed="0.6s"
     />
 
+    <Text p="1rem " textAlign="center" color="white">
+      FISE 2021 by UCL and IBM
+    </Text>
+    <Stack>
+      <Icon name="ibm" color="white" w="200px" h="50px" m="1rem" />
+      <Icon name="nhsX" color="white" w="200px" h="60px" m="1rem" />
+      <Icon name="ucl" color="gray.400" w="200px" h="50px" m="1rem" />
+    </Stack>
+
     <Stack p="1rem" color="gray.300">
       <Text textAlign="center">
         In association with IBM, NHS & University College London
       </Text>
-      <Text textAlign="center">
-        Supervised by John McNamara, Dean Mohamedally
+      <Text
+        textAlign="center"
+        maxWidth="35em"
+        color="gray.500"
+        wordBreak="true"
+      >
+        The software is an early proof of concept for development purposes and
+        should not be used as-is in a live environment without further
+        redevelopment and/or testing. No warranty is given and no real data or
+        personally identifiable data should be stored. Usage and its liabilities
+        are your own.
       </Text>
     </Stack>
   </Flex>
