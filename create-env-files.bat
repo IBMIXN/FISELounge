9bbffdf76b203662e83d6a82f5b531e0043d158f
DanJav <zcabjav@ucl.ac.uk>
2021-03-30 02:30:37 +0100

    feat #28 fixed create-env-var.bat bug

--- create-env-files.bat
+++ create-env-files.bat
@@ -1,7 +1,7 @@
 @echo off
 
-copy /d /y "app\env.local.example" "app\env.local"
-copy /d /y "server\env.local.example" "server\env.local"
+copy /d /y "app\.env.local.example" "app\.env.local"
+copy /d /y "server\.env.local.example" "server\.env.local"
 
 echo =====================================================================
 echo   YOU HAVE NOW CRETED .ENV.FILES FOR SERVER AND APP - FILL THEM IN
